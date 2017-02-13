f3847317a43367ceb361c88a0d4bcd20503fbcb5
Jordanh232 <johho@ucdavis.edu>
2017-02-12 21:59:15 -0800

    Working Login

--- app/server.py
+++ app/server.py
@@ -1,19 +1,100 @@
 #!/usr/bin/python
 import sys, helpers
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session, flash, url_for, redirect, abort ,g
 from cStringIO import StringIO
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import login_user , logout_user , current_user , login_required, LoginManager
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/postgres'
+db = SQLAlchemy(app)
 
-@app.route('/')
-def hello():
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column('user_id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(20), unique=True, index=True)
+    password = db.Column('password', db.String(10))
+    email = db.Column('email', db.String(50), unique=True, index=True)
+    #registered_on = db.Column('registered_on', db.DateTime)
+
+    def __init__(self, username, password, email):
+        self.username = username
+        self.password = password
+        self.email = email
+        #self.registered_on = datetime.utcnow()
+
+    #def is_authenticated(self):
+     #   return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.username)
+
+db.create_all()
+db.session.commit()
+
+
+@app.route('/home')
+def home():
     return render_template("home.html")
+
 
 @app.route('/execute')
 def execute():
     code = request.args.get('code', 0, type=str)
     output = helpers.run_code(code)
-    return jsonify(output = output)
+    return jsonify(output=output)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    user = User(request.form['username'], request.form['password'], request.form['email'])
+    db.session.add(user)
+    db.session.commit()
+    flash('User successfully registered')
+    return redirect(url_for('login'))
+
+
+@app.route('/',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    username = request.form['username']
+    password = request.form['password']
+    registered_user = User.query.filter_by(username=username,password=password).first()
+    if registered_user is None:
+        flash('Username or Password is invalid' , 'error')
+        return redirect(url_for('login'))
+    login_user(registered_user)
+    flash('Logged in successfully')
+    return redirect(request.args.get('next') or url_for('home'))
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('/login'))
+
+@app.before_request
+def before_request():
+    g.user = current_user
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 if __name__ == '__main__':
+    app.secret_key = 'Thisissecret'
     app.run(debug = True)
